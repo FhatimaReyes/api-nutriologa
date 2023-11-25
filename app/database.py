981d336ad86cbf9f572e5241e2ad342af3f26b62
FhatimaReyes <fhareal10@gmail.com>
2023-11-24 22:06:40 -0600

    update database_url

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/nutriologa"
+DATABASE_URL = "postgresql://postgres:CG--fG5f6a5-36a*5agFE3AGeGfEFD-2@roundhouse.proxy.rlwy.net:22059/railway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
